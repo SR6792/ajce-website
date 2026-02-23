6a3a47bccf9dd70da78e9e0832704bf1048183fe
SR6792 <sooraj.2050in@gmail.com>
2026-02-23 16:00:58 +0530

    Setup GitHub Pages deployment for frontend

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,7 +6,7 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
    <React.StrictMode>
-      <BrowserRouter>
+      <BrowserRouter basename="/ajce-website">
          <App />
       </BrowserRouter>
    </React.StrictMode>,
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+   base: '/ajce-website/',
    plugins: [react()],
    server: {
       proxy: {
